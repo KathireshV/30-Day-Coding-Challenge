03182679ddd05d52fd947f4d82e986e617631e92
KathireshV <125338548+KathireshV@users.noreply.github.com>
2023-11-07 20:11:19 +0530

    Create CodeChef - Chef Fantasy.cpp

---
+++ Day 1/CodeChef - Chef Fantasy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n>0)
+	{
+	    int a;
+	    cin>>a;
+	    int p=1;
+	    cout<<((a*a)-a)<<endl;
+	    n--;
+	}
+	return 0;
+}
