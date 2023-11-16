b64bdc54b19f09411950fe23eeae1049f0eaf800
KathireshV <125338548+KathireshV@users.noreply.github.com>
2023-11-16 21:30:27 +0530

    Create CodeChef-Rivalry.cpp

---
+++ Day 6/CodeChef-Rivalry.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1,r2,d1,d2;
+    cin>>r1>>r2>>d1>>d2;
+    if(r1+d1 > r2+d2)
+    {
+        cout<<"Dominater";
+    }
+    else
+    {
+        cout<<"Everule";
+    }
+	return 0;
+}
