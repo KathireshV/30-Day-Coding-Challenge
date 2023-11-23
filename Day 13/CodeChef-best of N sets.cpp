5967c01bee7811e82d32b268bc83e0ecb60be671
KathireshV <125338548+KathireshV@users.noreply.github.com>
2023-11-23 20:49:05 +0530

    Update CodeChef-best of N sets.cpp

--- Day 13/CodeChef-best of N sets.cpp
+++ Day 13/CodeChef-best of N sets.cpp
@@ -1,1 +1,21 @@
+int main() 
+{
+    int n;
+    cin>>n;
+    int a,b;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        if(b>a)
+        {
+            cout<<(b*2)-1<<endl;
+        }
+        else
+        {
+            cout<<(a*2)-1<<endl;
+            
+        }
+    }
+	return 0;
+}
 
